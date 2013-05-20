542892a602ce6d40253e0807367ca173b19de951
Joel Jackson <jackson.joel@gmail.com>
2013-05-19 17:33:22 -0700

    Initial gem scaffold.

---
+++ lib/sinatra/promises.rb
@@ -0,0 +1,7 @@
+require "sinatra/promises/version"
+
+module Sinatra
+  module Promises
+    # Your code goes here...
+  end
+end
---
+++ lib/sinatra/promises/version.rb
@@ -0,0 +1,5 @@
+module Sinatra
+  module Promises
+    VERSION = "0.0.1"
+  end
+end
